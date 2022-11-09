a135d553ade8abe4721e7d4e35432a47acc66fb1
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2022-11-09 11:47:25 +0300

    Update gaomon_keys.sh
    adding new key functions

--- gaomon_keys.sh
+++ gaomon_keys.sh
@@ -6,5 +6,7 @@ xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 3 "key +ctrl - -ctrl"
 xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 8 "key +ctrl + -ctrl"
 xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 9 "key +ctrl z -ctrl"
 xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 10 "key +ctrl +shift z -shift -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 11 "key +ctrl e -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 12 "key +ctrl +shift e -shift -ctrl"
 # GAOMON S620 PEN KEY SETTINGS
 xsetwacom --set 'GAOMON Gaomon Tablet Pen stylus' Button 2 "key del"
